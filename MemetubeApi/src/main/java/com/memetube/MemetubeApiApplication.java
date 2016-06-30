7de4eb90e4c4fbab7d0e6cc7592cc0da32dfb238
lazarov1994 <lazarov686@gmail.com>
2016-06-30 21:35:40 +0300

    star project

---
+++ MemetubeApi/src/main/java/com/memetube/MemetubeApiApplication.java
@@ -0,0 +1,12 @@
+package com.memetube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemetubeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemetubeApiApplication.class, args);
+	}
+}
